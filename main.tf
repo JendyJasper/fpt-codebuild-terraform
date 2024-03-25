38fd6ec000624d90e88b7d2de8105aa261abc777
ChijinduIG.HIS <chijinduighis@LPP00306402B.fsoft.fpt.vn>
2024-03-25 10:46:47 +0700

    used map of strings for var and added ecr registry

--- main.tf
+++ main.tf
@@ -38,19 +38,80 @@ module "iam" {
 
 module "codebuild_dev" {
   source = "./codebuild/"
+  codebuild-name = var.codebuild-name["dev"]
   codebuild-role = module.iam.codebuild-role-arn
   git-location = var.git-location
-  buildspec-file = var.buildspec-dev
+  buildspec-file = var.buildspec["dev"]
   codebuild-description = "codebuild project for the dev environment"
 
 }
 
 module "codebuild_prod" {
   source = "./codebuild/"
-  codebuild-name = "fpt-flask-build-prod"
+  codebuild-name = var.codebuild-name["prod"]
   codebuild-role = module.iam.codebuild-role-arn
   git-location = var.git-location
-  buildspec-file = var.buildspec-prod
+  buildspec-file = var.buildspec["prod"]
   codebuild-description = "codebuild project for the prod environment"
 
+}
+
+module "ecr-prod" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.0.0"
+
+  repository_name = var.repository-name["prod"]
+
+  repository_read_write_access_arns = ["arn:aws:iam::571207880192:user/terraform"]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+module "ecr-dev" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.0.0"
+
+  repository_name = var.repository-name["dev"]
+
+  repository_read_write_access_arns = ["arn:aws:iam::571207880192:user/terraform"]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Environment = "dev"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -6,14 +6,27 @@ variable "git-location" {
   default = "https://github.com/JendyJasper/fpt-flask-app-spec-files.git"
 }
 
-variable "buildspec-dev" {
-  default = "buildspec_dev.yml"
+variable "buildspec" {
+  type = map(string)
+  default = {
+    "prod" : "buildspec_prod.yml"
+    "dev" : "buildspec_dev.yml"
+  }
 }
 
-variable "buildspec-prod" {
-  default = "buildspec_prod.yml"
-}
 
 variable "codebuild-name" {
-  default = "codebuild-dev"
+  type = map(string)
+  default = {
+    "prod" : "codebuild-prod"
+    "dev" : "codebuild-dev"
+  }
+}
+
+variable "repository-name" {
+  type = map(string)
+  default = {
+    "prod" : "fpt-prod"
+    "dev" : "fpt-dev"
+  }
 }
